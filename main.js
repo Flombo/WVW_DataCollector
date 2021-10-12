e01873d16c9752e8ac1d01f15c384dd8e91a8f21
Florian <florian-pfuetzenreuter@hotmail.de>
2021-10-12 14:10:23 +0200

    added wvw-api call loop

--- main.js
+++ main.js
@@ -2,7 +2,28 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const request=require('request');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname));
 
+app.get('/', (req, response)=>{
+    const wvwURL = "https://api.guildwars2.com/v1/wvw/matches.json";
 
+    request.get(wvwURL, (err,res,body) => {
+        if(err) console.log(err);
+        response.send(body);
+    })
+})
 
-server.listen(3000);
+function callAPI(){
+    const wvwURL = "https://api.guildwars2.com/v1/wvw/matches.json";
+
+    request.get(wvwURL, (err,res,body) => {
+        if(err) console.log(err);
+        console.log(body);
+    })
+}
+
+server.listen(3000, "localhost", null, () => {
+    setInterval(callAPI, 1000);
+});

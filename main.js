87027dea3ad12183146347ed14b4927a9f9eea39
Flombo <florian-pfuetzenreuter@hotmail.de>
2021-11-10 16:47:05 +0100

    Added cronjob amd producers for every eu match

--- main.js
+++ main.js
@@ -4,54 +4,68 @@ const http = require('http');
 const server = http.createServer(app);
 const request=require('request');
 const { Kafka } = require('kafkajs')
+const cron = require('node-cron');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
 
 
 const kafka = new Kafka({
-    clientId: 'my-app',
+    clientId: 'producersite',
     brokers: ['localhost:9092']
-})
+});
 
-let producer;
+let producerMatchMap = new Map();
+
+const wvwURL = "https://api.guildwars2.com/v2/wvw/matches/";
 
 async function initProducer(){
-    producer = kafka.producer()
-    await producer.connect()
-}
+    let producerMatch2_1 = kafka.producer();
+    await producerMatch2_1.connect();
+    producerMatchMap.set('2-1', producerMatch2_1);
 
-async function initConsumer(){
-    const consumer = kafka.consumer({ groupId: 'test-group' })
+    let producerMatch2_2 = kafka.producer();
+    await producerMatch2_2.connect();
+    producerMatchMap.set('2-2', producerMatch2_2);
 
-    await consumer.connect()
-    await consumer.subscribe({ topic: 'wvwDataCollector', fromBeginning: true })
+    let producerMatch2_3 = kafka.producer();
+    await producerMatch2_3.connect();
+    producerMatchMap.set('2-3', producerMatch2_3);
 
-    await consumer.run({
-        eachMessage: async ({ topic, partition, message }) => {
-            console.log({
-                value: message.value.toString(),
-            })
-        },
-    })
+    let producerMatch2_4 = kafka.producer();
+    await producerMatch2_4.connect();
+    producerMatchMap.set('2-4', producerMatch2_4);
+
+    let producerMatch2_5 = kafka.producer();
+    await producerMatch2_5.connect();
+    producerMatchMap.set('2-5', producerMatch2_5);
 }
 
 initProducer();
-initConsumer();
 
 function callAPI(){
-    const wvwURL = "https://api.guildwars2.com/v2/wvw/matches/2-1.json";
+    producerMatchMap.forEach((producer, topicName) => {
+        request.get(wvwURL + topicName + '.json', async (err,res,body) => {
+            if(err) console.log(err);
 
-    request.get(wvwURL,async (err,res,body) => {
-        if(err) console.log(err);
-        await producer.send({
-            topic: 'wvwDataCollector',
-            messages: [
-                { value: body },
-            ],
-        })
-    })
+            let bodyJSON = JSON.parse(body);
+            bodyJSON['timestamp'] = Date.now();
+
+            await producer.send({
+                topic: topicName,
+                messages: [
+                    { value: JSON.stringify(bodyJSON) },
+                ],
+            });
+        });
+    });
 }
 
-server.listen(3000, "localhost", null, () => {
-    setInterval(callAPI, 1000);
+cron.schedule('* * * * * *', function() {
+    callAPI();
 });
+
+app.get('/', (req, res) => {
+   res.send('hello');
+});
+
+server.listen(3000, '141.28.73.146');

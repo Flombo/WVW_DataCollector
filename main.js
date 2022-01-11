be96b7aa5e001e7b15ad41e266f6f1a89b8bc495
Flombo <florian-pfuetzenreuter@hotmail.de>
2022-01-11 12:17:43 +0100

    Added an call for the maps-API for retrieving the map-names and added them to the JSON. Split the three call functions to single functions.

--- main.js
+++ main.js
@@ -49,6 +49,7 @@ initProducer();
 /***
  * calls foreach producer the match-api and the worlds-api.
  * the worlds-attribute of the match-object will be overridden by the fetched worlds.
+ * foreach map, the maps-API will be called for retrieving and adding the name to each map.
  * In the end the match-object will be sent to kafka.
  */
 function callAPI(){
@@ -62,45 +63,10 @@ function callAPI(){
                     body += data;
                 });
 
-                res.on("end", () => {
+                res.on("end", async () => {
                     try {
                         const matchJSON = JSON.parse(body);
-
-                        const redID = matchJSON.worlds.red;
-                        const blueID = matchJSON.worlds.blue;
-                        const greenID = matchJSON.worlds.green;
-
-                        const worldURL = `https://api.guildwars2.com/v2/worlds?ids=${redID},${blueID},${greenID}`;
-
-                        https.get(worldURL, res => {
-
-                            let worldsBody = "";
-
-                            res.on("data", (data) => {
-                                worldsBody += data;
-                            });
-
-                            res.on("end", async () => {
-
-                                const worlds = JSON.parse(worldsBody);
-
-                                if (worlds.length === 3) {
-                                    matchJSON.worlds.red = worlds[0];
-                                    matchJSON.worlds.blue = worlds[1];
-                                    matchJSON.worlds.green = worlds[2];
-                                }
-
-                                await producer.send({
-                                    topic: topicName,
-                                    messages: [
-                                        {
-                                            value : JSON.stringify(matchJSON)
-                                        }
-                                    ]
-                                });
-                            });
-
-                        });
+                        await callWorldsAPI(matchJSON, producer, topicName);
                     } catch (error) {
                         console.error(error.message);
                     }
@@ -113,8 +79,89 @@ function callAPI(){
     });
 }
 
+async function callWorldsAPI(matchJSON, producer, topicName) {
+    const redID = matchJSON.worlds.red;
+    const blueID = matchJSON.worlds.blue;
+    const greenID = matchJSON.worlds.green;
+
+    const worldURL = `https://api.guildwars2.com/v2/worlds?ids=${redID},${blueID},${greenID}`;
+
+    https.get(worldURL, res => {
+
+        let worldsBody = "";
+
+        res.on("data", (data) => {
+            worldsBody += data;
+        });
+
+        res.on("end", async () => {
+            try {
+                const worlds = JSON.parse(worldsBody);
+
+                if (worlds.length === 3) {
+                    matchJSON.worlds.red = worlds[0];
+                    matchJSON.worlds.blue = worlds[1];
+                    matchJSON.worlds.green = worlds[2];
+                }
+
+                await callMapAPI(matchJSON, producer, topicName)
+            } catch (exception) {
+                console.error(exception);
+            }
+        });
+    });
+}
+
+async function callMapAPI(matchJSON, producer, topicName) {
+
+    const maps = matchJSON.maps;
+    const mapURL = `https://api.guildwars2.com/v2/maps?ids=${maps[0].id},${maps[1].id},${maps[2].id},${maps[3].id}`
+
+    https.get(mapURL, res => {
+
+        let mapsBody = "";
+
+        res.on("data", (data) => {
+            mapsBody += data;
+        });
+
+        res.on("end", async () => {
+            try {
+                mapsBody = JSON.parse(mapsBody);
+
+                if (mapsBody.length === 4) {
+                    matchJSON.maps[0].name = mapsBody[0].name;
+                    matchJSON.maps[1].name = mapsBody[1].name;
+                    matchJSON.maps[2].name = mapsBody[2].name;
+                    matchJSON.maps[3].name = mapsBody[3].name;
+                }
+
+                await sendMessageToKafka(matchJSON, producer, topicName);
+            } catch (exception) {
+                console.error(exception);
+            }
+        });
+    });
+}
+
+async function sendMessageToKafka(matchJSON, producer, topicName) {
+    try {
+
+        await producer.send({
+            topic: topicName,
+            messages: [
+                {
+                    value: JSON.stringify(matchJSON)
+                }
+            ]
+        });
+    } catch (exception) {
+        console.error(exception);
+    }
+}
+
 /*cron job calls every half minute the callApi function*/
-cron.schedule('*/30 * * * * *', function() {
+cron.schedule('*/40 * * * * *', function() {
     callAPI();
 });
 
